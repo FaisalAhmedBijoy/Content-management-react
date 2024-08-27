54fc2ae2288f0dcf7e306f731dfaf03ea155a90b
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2024-08-28 00:41:46 +0600

    update code

---
+++ git_commit.sh
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "update code"
+git remote add origin https://github.com/FaisalAhmedBijoy/Content-management-react.git
+git push --set-upstream origin master
--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const projectName = 'Content Management System'
+const projectDescription = 'Project Development by React + Vue'
+const date = new Date().getFullYear()
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Management System</h1>
+        <h2>Developed in React</h2>
+
+        <h3>Project Name: {projectName}</h3>
+        <h2>Description: {projectDescription}</h2>
+        <h3>Time: {date} </h3>
+
+ 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
