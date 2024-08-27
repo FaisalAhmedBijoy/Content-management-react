540be41e64663e4c6271c22100b5da9ebf719944
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2024-08-28 01:30:44 +0600

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import './App.css'
 const projectName = 'Content Management System'
 const projectDescription = 'Project Development by React + Vue'
 const date = new Date().getFullYear()
+const headingStyle ={
+  backgroundColor: "purple",
+  color: 'white',
+  textAlign: "center",
+  padding: '15px'
+}
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,9 +21,9 @@ function App() {
         <h1>Management System</h1>
         <h2>Developed in React</h2>
 
-        <h3>Project Name: {projectName}</h3>
-        <h2>Description: {projectDescription}</h2>
-        <h3>Time: {date} </h3>
+        <h3 style ={{color : 'red', fontSize : "3rem"}} >Project Name: {projectName}</h3>
+        <h2 style ={headingStyle} >Description: {projectDescription}</h2>
+        <h3 className = "demoHeadingStyle" >Time: {date} </h3>
 
  
       </div>
